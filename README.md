83738778c335aca4a54d6fa396e0f91a6fb4b3d3
Vidya1711 <codejourneywithme@gmail.com>
2024-02-10 10:06:48 +0530

    project completed

---
+++ README.md
@@ -0,0 +1,15 @@
+# Chest_Cancer_Classification_Mlflow
+
+
+MLFLOW_TRACKING_URI=https:uri 
+MLFLOW_TRACKING_USERNAME=username
+MLFLOW_TRACKING_PASSWORD=password
+python script.py
+
+
+
+export MLFLOW_TRACKING_URI=uri
+
+export MLFLOW_TRACKING_USERNAME=username 
+
+export MLFLOW_TRACKING_PASSWORD=password
---
+++ main.py
@@ -0,0 +1,53 @@
+from cnnClassifier import logger 
+from cnnClassifier.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+from cnnClassifier.pipeline.prepare_base_model_pipe import PrepareBaseModelTrainingPipeline
+from cnnClassifier.pipeline.model_training_pipeline import ModelTrainingPipeline
+from cnnClassifier.pipeline.evaluation_pipeline import ModelEvaluationPipeline
+STAGE_NAME = "Data Ingestion stage"
+
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Prepare base model"
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Model training stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+
+STAGE_NAME = "Model evaluation stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelEvaluationPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
